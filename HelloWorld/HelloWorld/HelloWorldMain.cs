2080fcaaa8eaad24c275a2fe92d67a94b373c931
aokomoriuta_lab <internal@enmps.net>
2012-04-13 18:28:37 +0900

    HelloWorldを追加

---
+++ HelloWorld/HelloWorld/HelloWorldMain.cs
@@ -0,0 +1,24 @@
+﻿namespace LWisteria.StudiesOfSlimDX.HelloWorld
+{
+	/// <summary>
+	/// メインクラス
+	/// </summary>
+	partial class HelloWorldMain : System.Windows.Application
+	{
+		/// <summary>
+		/// 最上位ウインドウ
+		/// </summary>
+		MainWindow mainWindow;
+
+		/// <summary>
+		/// メインクラスを開始
+		/// </summary>
+		public HelloWorldMain()
+		{
+			// 最上位ウインドウを作成して設定
+			this.mainWindow = new MainWindow();
+			base.MainWindow = this.mainWindow;
+			base.MainWindow.Show();
+		}
+	}
+}
